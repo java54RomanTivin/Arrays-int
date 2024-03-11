52be26c2518e782c8b206f1f39f61d65e5ee75eb
Yuragranovsky <yuragranovsky@gmail.com>
2024-03-11 21:59:39 +0200

    TODO's for HW #6

---
+++ src/telran/util/ArraysInt.java
@@ -0,0 +1,26 @@
+package telran.util;
+
+public class ArraysInt {
+	public static int[] addNumber(int[] array, int number) {
+		
+		//TODO
+		//apply method copyOf of class Arrays
+		return null;
+	}
+	public static int[] insertNumber(int[] array, int index, int number) {
+		//TODO
+		//apply method arraycopy of class System
+		return null;
+	}
+	public static int[] removeNumber(int[] array, int index) {
+		//TODO
+		//apply method arraycopy of class System
+		return null;
+	}
+	public static int[] insertSorted(int[] sortedArray, int number) {
+		//TODO
+		//insert number into sorted array with keeping the sorted order
+		//apply method binarySearch of the class Arrays
+		return null;
+	}
+}
